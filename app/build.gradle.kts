15053aa5e765841fc824374baa6e1ccdeac6801d
Raul Bache <raul@bache.se>
2023-12-18 13:41:01 +0100

    Works

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -44,12 +44,11 @@ android {
 
 dependencies {
     // Import the Compose BOM
-    implementation(platform("androidx.compose:compose-bom:2023.10.01"))
+    implementation(platform("androidx.compose:compose-bom:2023.06.01"))
     // Import Material Design 3 library
     implementation("androidx.compose.foundation:foundation")
     implementation("androidx.activity:activity-compose")
     implementation("androidx.compose.material3:material3")
-    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.10.0")
 }
